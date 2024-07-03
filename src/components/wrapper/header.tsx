fce8340a9d734721efc835ec4ca1d54ef817154f
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-03 09:45:11 -0700

    Created wrapper components (header/footer) that get reused between different views/pages.

---
+++ src/components/wrapper/header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import WeightChart from '../../chart.gif';
+
+function Header(){
+  return (
+    <header>
+        <img src={WeightChart} id="weight-chart" alt="BMI weight chart"/>
+        <h1><a href="../../index.tsx">SkillSoft Weight Tracker</a></h1>
+        <nav>
+            <ul>
+                <li><a href="../../index.tsx">home</a></li>
+                <li><a href="enterweight.html">enter weight</a></li>
+                <li><a href="myweights.html">my weight</a></li>
+                <li><a href="teamweights.html">team weights</a></li>
+            </ul>
+        </nav>
+    </header>
+  )
+};
+
+export default Header;
