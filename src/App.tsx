a87d399de076fbd75eae156032864d558d460521
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-03 10:11:36 -0700

    Added Customers route to App. Also added comments to better explain code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './styles.css';
+// Home component contains the Header, Footer, and Container (includes Aside & unique Main)
 import Home from './components/home/home';
+// Customers component contains the Header, Footer, and Container (includes Aside & unique Main)
+import Customers from './components/customers/customers';
 import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
         <BrowserRouter>
-            <Route path="/" component={Home} />
+            {/* exact path route used for first entry only (generally default entrypoint) */}
+            <Route exact path="/" component={Home} />
+            <Route path="/customers" component={Customers} />
         </BrowserRouter>
     </div>
   );
