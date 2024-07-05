b577d09054106be7f4758e4682974de4e5ee9b8a
Nicole-Rene Newcomb <n.newcomb@elsevier.com>
2024-07-04 18:38:13 -0700

    Created fetch request to retrieve customer entries from json file. Then mapped customers returned to display within individual div tags with the key indicating the entry number.

--- src/components/customers/main.tsx
+++ src/components/customers/main.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function Main() {
-    return (
-		<main>
-			<h2>Customers List</h2>
-		</main>
-    )
+type MainProps = {
+	username?: string;
+	password?: string;
+}
+
+type MainState = {
+	customers: any[]
+}
+
+class Main extends Component<MainProps, MainState> {
+	constructor(props: MainProps) {
+		super(props);
+		this.state = {
+			customers: []
+		}
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:3030/customers')
+			.then(data => {
+				return data.json();
+			})
+			.then(resolvedData => {
+				this.setState({customers: resolvedData})
+			})
+	};
+
+	render() {
+
+		return (
+			<main>
+				<h2>Customers List</h2>
+				{
+					this.state.customers.map((customer, i) => (
+					<div key={i}>
+						{customer.username}&nbsp;
+						{customer.password}
+					</div>))
+				}
+
+			</main>
+		)
+	}
+
 }
 
 export default Main;
